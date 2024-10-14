0ed0328ecbab066c1e546d99627ecb1cb71a12e4
Shivank282 <shivankbhargav98@gmail.com>
2024-10-14 09:28:15 +0530

    Add files via upload

---
+++ votingchacker.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main(){
+    int a;
+    printf("enter your age");
+    scanf("%d",a);
+    if(a>=18)
+    {
+        printf("you are eligibal for vote.");
+}
+else
+{
+    printf("you are not eligibal for vote.");
+    }
+}
